556e633da7fbe554363736d6c5254078fd131ec4
EllisFitzUSM <ellis.fitzgerald@maine.edu>
2025-03-31 22:02:41 -0400

    Basic Main file to make it easier to test basic functionalities.

---
+++ Iteration1/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Basic Entry Point to Test Child Immunization Functionality
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Child Immunizations");
+    }
+}
