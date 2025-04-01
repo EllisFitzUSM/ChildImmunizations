393c54c20ae509fa1a90f1f69008dc0e11d0938c
Mason Beale <mason.beale@maine.edu>
2025-04-01 15:32:01 -0400

    Basic headers for clinic, patient, and vaccine

--- Iteration1/Clinic.java
+++ Iteration1/Clinic.java
@@ -1,1 +1,11 @@
+/*
+ * Clinic.java
+ * @author:
+ * @version:
+ * Basic Clinic class that has little functionality at this point.
+ * This class will be used to manage the clinic's operations..
+ */
+public class Clinic {
 
+    
+}
